5cd146e1d71bbe05991c68ac277f7d2bbb2ce3ae
amitphalwadiya09 <151755174+amitphalwadiya09@users.noreply.github.com>
2024-02-28 21:17:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 //your JS code here. If required.
-cy.visit(baseUrl).then(() => {
-    // Get the current URL using cy.url()
-    cy.url().then(currentURL => {
-        // Calculate the length of the URL
-        const urlLength = currentURL.length;
-
-        // Display the length of the URL using alert
-        alert(`The length of the URL is: ${urlLength}`);
-    });
-});
+var url=window.prompt()
+let length=url.length;
+alert(`The length of the URL is: ${length}`)
